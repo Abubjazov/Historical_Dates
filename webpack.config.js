d962e7f94c5cef0523b5c0fe3e6cf005ece7da65
Radik Abubjazov <abubjazov@mail.ru>
2025-09-12 18:47:30 +0300

    dev: added linters & webpack setups

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.tsx',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: true,
+              modules: {
+                namedExport: false,
+              },
+            },
+          },
+          'sass-loader',
+        ],
       },
     ],
   },
@@ -17,17 +40,12 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   devServer: {
     static: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000,
+    port: 3000,
     open: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-};
+}
